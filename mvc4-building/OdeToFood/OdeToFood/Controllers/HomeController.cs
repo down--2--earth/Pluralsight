3323c4d213c93b889917c1ff93f9b802c8a17c42
Mahesh Dharmasena <mahesh.ksl@gmail.com>
2015-02-12 12:33:38 +0530

    Inital solution

---
+++ mvc4-building/OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+
+            var message = String.Format("{0}::{1}::{2}", controller, action, id);
+
+            //ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            ViewBag.Message = message;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your app description page.";
+            //ViewBag.Location = "Colombo, Sri Lanka";
+
+            var model = new AboutModel();
+            model.Name = "Mahesh";
+            model.Location = "Colombo";
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
