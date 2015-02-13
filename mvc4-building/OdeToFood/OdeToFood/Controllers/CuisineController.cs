6d528a0e5b6499b2f141f868a561634c2499fc91
Mahesh Dharmasena <mahesh.ksl@gmail.com>
2015-02-13 15:26:22 +0530

    Action Filters

--- mvc4-building/OdeToFood/OdeToFood/Controllers/CuisineController.cs
+++ mvc4-building/OdeToFood/OdeToFood/Controllers/CuisineController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OdeToFood.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,21 +7,35 @@ using System.Web.Mvc;
 
 namespace OdeToFood.Controllers
 {
+    [Log]
     public class CuisineController : Controller
     {
         //
         // GET: /Cuisine/
+        //[HttpPost]
+        //public ActionResult Search(string name = "french")
+        //{
+        //    return Content(Server.HtmlEncode(name));
+        //    //return RedirectToAction("Index", "Home", new { name = name });
 
+        //    //return RedirectToRoute("Default", new { controller = "Home", action = "About" });
+
+        //    //return File(Server.MapPath("~/Content/site.css"), "text/css");
+
+        //    //return Json(new { Message = Server.HtmlEncode(name), Name = "Mahesh" }, JsonRequestBehavior.AllowGet);
+        //}
+
+        //[HttpGet]
+        //public ActionResult Search()
+        //{
+        //    return Content("Hello HttpGet");
+        //}
+
+        //[Authorize]
         public ActionResult Search(string name = "french")
         {
-            //return Content(Server.HtmlEncode(name));
-            //return RedirectToAction("Index", "Home", new { name = name });
-
-            //return RedirectToRoute("Default", new { controller = "Home", action = "About" });
-
-            //return File(Server.MapPath("~/Content/site.css"), "text/css");
-
-            return Json(new { Message = Server.HtmlEncode(name), Name = "Mahesh" }, JsonRequestBehavior.AllowGet);
+            throw new Exception(" Bad Exception");
+            return Content(Server.HtmlEncode(name));
         }
 
     }
