ff1532d86a2da1d45f0d32a69e83b7a5cccde601
Mahesh Dharmasena <mahesh.ksl@gmail.com>
2015-02-17 16:22:39 +0530

    Creating a Review

--- mvc4-building/OdeToFood/OdeToFood/Controllers/ReviewsController.cs
+++ mvc4-building/OdeToFood/OdeToFood/Controllers/ReviewsController.cs
@@ -1,167 +1,85 @@
-﻿//using OdeToFood.Models;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
-//namespace OdeToFood.Controllers
-//{
-//    public class ReviewsController : Controller
-//    {
-//        [ChildActionOnly]
-//        public ActionResult BestReview()
-//        {
-//            var bestReview = from r in _reviews
-//                             orderby r.Rating descending
-//                             select r;
-//            return PartialView("_Review", bestReview.First());
-//        }
-//        //
-//        // GET: /Reviews/
+namespace OdeToFood.Controllers
+{
+    public class ReviewsController : Controller
+    {
 
-//        public ActionResult Index()
-//        {
-//            var model = from r in _reviews
-//                        orderby r.Country
-//                        select r;
+        OdeFoodDb _db = new OdeFoodDb();
 
-//            return View(model);
-//        }
+        //[ChildActionOnly]
+        //public ActionResult BestReview()
+        //{
+        //    var bestReview = null;
 
-//        //
-//        // GET: /Reviews/Details/5
+        //    return PartialView("_Review", bestReview.First());
+        //}
+        //
+        // GET: /Reviews/
 
-//        public ActionResult Details(int id)
-//        {
-//            return View();
-//        }
+        public ActionResult Index([Bind(Prefix = "id")] int restaurantId)
+        {
+            var restaurant = _db.Restaurants.Find(restaurantId);
 
-//        //
-//        // GET: /Reviews/Create
+            if (restaurant != null)
+            {
+                return View(restaurant);
+            }
 
-//        public ActionResult Create()
-//        {
-//            return View();
-//        }
+            return HttpNotFound();
+        }
 
-//        //
-//        // POST: /Reviews/Create
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+            base.Dispose(disposing);
+        }
 
-//        [HttpPost]
-//        public ActionResult Create(FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add insert logic here
 
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
 
-//        //
-//        // GET: /Reviews/Edit/5
+        //static List<RestaurantReview> _reviews = new List<RestaurantReview>
+        //{
+        //    new RestaurantReview 
+        //    {
+        //        Id = 1,
+        //        Name = "A",
+        //        City = "A",
+        //        Country = "A",
+        //        Rating = 5
+        //    },
 
-//        public ActionResult Edit(int id)
-//        {
-//            var review = _reviews.Single(r => r.Id == id);
-//            return View(review);
-//        }
+        //    new RestaurantReview 
+        //    {
+        //        Id = 2,
+        //        Name = "B",
+        //        City = "B",
+        //        Country = "B",
+        //        Rating = 5
+        //    },
 
-//        //
-//        // POST: /Reviews/Edit/5
+        //    new RestaurantReview 
+        //    {
+        //        Id = 3,
+        //        Name = "C",
+        //        City = "C",
+        //        Country = "C",
+        //        Rating = 5
+        //    },
 
-//        [HttpPost]
-//        public ActionResult Edit(int id, FormCollection collection)
-//        {
-//            //try
-//            //{
-//            //    // TODO: Add update logic here
+        //    new RestaurantReview 
+        //    {
+        //        Id = 4,
+        //        Name = "D",
+        //        City = "D",
+        //        Country = "D",
+        //        Rating = 5
+        //    }
 
-//            //    return RedirectToAction("Index");
-//            //}
-//            //catch
-//            //{
-//            //    return View();
-//            //}
-
-//            var review = _reviews.Single(r => r.Id == id);
-
-//            if (TryUpdateModel(review))
-//            {
-//                return RedirectToAction("Index");
-//            }
-//            return View(review);
-//        }
-
-//        //
-//        // GET: /Reviews/Delete/5
-
-//        public ActionResult Delete(int id)
-//        {
-//            return View();
-//        }
-
-//        //
-//        // POST: /Reviews/Delete/5
-
-//        [HttpPost]
-//        public ActionResult Delete(int id, FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add delete logic here
-
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-
-//        static List<RestaurantReview> _reviews = new List<RestaurantReview>
-//        {
-//            new RestaurantReview 
-//            {
-//                Id = 1,
-//                Name = "A",
-//                City = "A",
-//                Country = "A",
-//                Rating = 5
-//            },
-
-//            new RestaurantReview 
-//            {
-//                Id = 2,
-//                Name = "B",
-//                City = "B",
-//                Country = "B",
-//                Rating = 5
-//            },
-
-//            new RestaurantReview 
-//            {
-//                Id = 3,
-//                Name = "C",
-//                City = "C",
-//                Country = "C",
-//                Rating = 5
-//            },
-
-//            new RestaurantReview 
-//            {
-//                Id = 4,
-//                Name = "D",
-//                City = "D",
-//                Country = "D",
-//                Rating = 5
-//            }
-
-//        };
-//    }
-//}
+        //};
+    }
+}
