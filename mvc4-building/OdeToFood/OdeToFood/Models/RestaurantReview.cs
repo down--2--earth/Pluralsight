c7bbd3d556fd57648e2f42585a219d0fa6431698
Mahesh Dharmasena <mahesh.ksl@gmail.com>
2015-02-18 16:26:57 +0530

    Custom Validation

--- mvc4-building/OdeToFood/OdeToFood/Models/RestaurantReview.cs
+++ mvc4-building/OdeToFood/OdeToFood/Models/RestaurantReview.cs
@@ -6,7 +6,31 @@ using System.Web;
 
 namespace OdeToFood.Models
 {
-    public class RestaurantReview
+    public class MaxWordsAttribute : ValidationAttribute
+    {
+        private readonly int _maxwords;
+
+        public MaxWordsAttribute(int maxwords) : base("{0} has too many words.")
+        {
+            _maxwords = maxwords;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                var valueAsString = value.ToString();
+                if (valueAsString.Split(' ').Length > _maxwords)
+                {
+                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+                    return new ValidationResult(errorMessage);
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+
+    public class RestaurantReview : IValidatableObject
     {
         public int Id { get; set; }
 
@@ -20,7 +44,16 @@ namespace OdeToFood.Models
 
         [Display(Name="User Name")]
         [DisplayFormat(NullDisplayText="anonymous")]
+        [MaxWords(1)]
         public string  ReviewerName { get; set; }
         public int RestaurantId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (ReviewerName.StartsWith("AA"))
+            {
+                yield return new ValidationResult("Invalid Input");
+            }
+        }
     }
 }
