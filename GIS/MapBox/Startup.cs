c5acccbf3a4b570fb2d2f097a1b0b304eb65b5b8
Mahesh Dharmasena <mahesh.ksl@gmail.com>
2015-03-04 09:35:33 +0530

    Added MapBox project

---
+++ GIS/MapBox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MapBox.Startup))]
+namespace MapBox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
