773bc059a47c2975118691cf14f29fc1dce09d9c
Mahesh Dharmasena <mahesh.ksl@gmail.com>
2015-03-23 17:53:21 +0530

    Form post

--- front-end-web-app-html5-javascript-css/front-end-web-app-html5-javascript-css/Controllers/HomeController.cs
+++ front-end-web-app-html5-javascript-css/front-end-web-app-html5-javascript-css/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using front_end_web_app_html5_javascript_css.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -25,6 +26,13 @@ namespace front_end_web_app_html5_javascript_css.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Echo(string searchPhrase = "Mahesh")
+        {
+            var echoModel = new EchoModel();
+            echoModel.EchoName = searchPhrase;
+            return View(echoModel);
         }
     }
 }
---
+++ front-end-web-app-html5-javascript-css/front-end-web-app-html5-javascript-css/Models/EchoModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace front_end_web_app_html5_javascript_css.Models
+{
+    public class EchoModel
+    {
+        public string EchoName { get; set; }
+    }
+}
